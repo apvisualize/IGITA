b378bb553ae43df16dc3a61e41e28bcad0783397
apvisualize <budimanhian@gmail.com>
2026-05-22 21:50:02 +0700

    fix: ganti metode kirim data ke fetch no-cors

--- register.js
+++ register.js
@@ -172,7 +172,6 @@
 
       stepText.textContent = 'Langkah ' + currentStep + ' dari ' + TOTAL_STEPS;
       modal.scrollTop = 0;
-      focusFirstInput(currentStep);
     }
 
     btnNext.addEventListener('click', () => {
